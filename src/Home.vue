aab66a1fa65b2d0a4dcf88071204f48d49255b0f
Lidong Guo <guolidong@genomics.cn>
2023-01-05 16:22:09 +0800

    update details

--- src/Home.vue
+++ src/Home.vue
@@ -205,6 +205,7 @@
             </el-row>
           </el-col>
           <el-col :span="6">
+            <div class=left_border>
             <el-row>
                 <!-- card begin-->
                 <el-col :span="24">
@@ -287,13 +288,14 @@
                 </el-col>
                 <!-- card end-->
             </el-row>
+            </div>
           </el-col>
         </el-row>
        </el-tab-pane>
        <el-tab-pane label="Tutorials" name='tutorial'>
         <h2>Tutorials to reproduce those atlases&#58;</h2>
         <el-collapse v-model="tid" accordion>
-            <el-collapse-item   title="E14-16h" name="1">
+            <el-collapse-item   title="Developing drosophila embryos and larvae: E14-16h" name="1">
               <div class='tutorial'>
                   <p>
                   To reproduce this atlas&#58;<br>
@@ -314,7 +316,7 @@
                   </p>
               </div>
             </el-collapse-item>
-            <el-collapse-item   title="E16-18h" name="2">
+            <el-collapse-item   title="Developing drosophila embryos and larvae: E16-18h" name="2">
               <div class='tutorial'>
                   <p>
                   To reproduce this atlas&#58;<br>
@@ -335,7 +337,7 @@
                   </p>
               </div>
             </el-collapse-item>
-            <el-collapse-item   title="L1" name="3">
+            <el-collapse-item   title="Developing drosophila embryos and larvae: L1" name="3">
               <div class='tutorial'>
                   <p>
                   To reproduce this atlas&#58;<br>
@@ -356,7 +358,7 @@
                   </p>
               </div>
             </el-collapse-item>
-            <el-collapse-item   title="L2" name="4">
+            <el-collapse-item   title="Developing drosophila embryos and larvae: L2" name="4">
               <div class='tutorial'>
                   <p>
                   To reproduce this atlas&#58;<br>
@@ -377,7 +379,7 @@
                   </p>
               </div>
             </el-collapse-item>
-            <el-collapse-item   title="L3" name="5">
+            <el-collapse-item   title="Developing drosophila embryos and larvae: L3" name="5">
               <div class='tutorial'>
                   <p>
                   To reproduce this atlas&#58;<br>
@@ -398,7 +400,7 @@
                   </p>
               </div>
             </el-collapse-item>
-            <el-collapse-item   title="Heart" name="6">
+            <el-collapse-item   title="Developing human heart" name="6">
               <div class='tutorial'>
                   <p>
                   To reproduce this atlas&#58;<br>
@@ -419,7 +421,7 @@
                   </p>
               </div>
             </el-collapse-item>
-            <el-collapse-item   title="Hypo Preoptic" name="7">
+            <el-collapse-item   title="Hypo Preoptic hypothalamic" name="7">
               <div class='tutorial'>
                   <p>
                   To reproduce this atlas&#58;<br>
@@ -428,10 +430,10 @@
                   mkdir hypo_preoptic<br>
                   cd hypo_preoptic<br>
                   wget -c http&#58;&#47;&#47;www.bgiocean.com&#47;vt3d_example&#47;download&#47;merfish&#47;hypo_preoptic.h5ad<br>
-                  wget -c http&#58;&#47;&#47;www.bgiocean.com&#47;vt3d_example&#47;download&#47;heart&#47;mesh.tar.gz<br>
+                  wget -c http&#58;&#47;&#47;www.bgiocean.com&#47;vt3d_example&#47;download&#47;merfish&#47;mesh.tar.gz<br>
                   tar -xzf mesh.tar.gz<br>
-                  wget -c http&#58;&#47;&#47;www.bgiocean.com&#47;vt3d_example&#47;download&#47;heart&#47;hypo_preoptic.atlas.json<br>
-                  wget -c http&#58;&#47;&#47;www.bgiocean.com&#47;vt3d_example&#47;download&#47;heart&#47;hypo_preoptic.coord.json<br>
+                  wget -c http&#58;&#47;&#47;www.bgiocean.com&#47;vt3d_example&#47;download&#47;merfish&#47;hypo_preoptic.atlas.json<br>
+                  wget -c http&#58;&#47;&#47;www.bgiocean.com&#47;vt3d_example&#47;download&#47;merfish&#47;hypo_preoptic.coord.json<br>
                   vt3d AtlasBrowser BuildAtlas -i hhypo_preoptic.h5ad -o HP_Atlas -c hypo_preoptic.atlas.json<br>
                   cd HP_Atlas &#38;&#38; vt3d AtlasBrowser LaunchAtlas -p 80<br>
                   <br>
@@ -442,118 +444,161 @@
             </el-collapse-item>
         </el-collapse  >
        </el-tab-pane>
-       <el-tab-pane label="More datasets description" name='data'>
-         <h2>More datasets description&#58;</h2>
+       <el-tab-pane label="Resources" name='data'>
+         <h3>Dataset description and data download&#58;</h3>
          <el-row>
            <el-col :span="2"><h4>name</h4></el-col>
-           <el-col :span="2"><h4>produced by</h4></el-col>
-           <el-col :span="5"><h4>.h5ad data</h4></el-col>
-           <el-col :span="5"><h4>.obj data</h4></el-col>
-           <el-col :span="10"><h4>reference</h4></el-col>
+           <el-col :span="2"><h4>spatial tech.</h4></el-col>
+           <el-col :span="6"><h4>.h5ad data</h4></el-col>
+           <el-col :span="3"><h4>.obj data</h4></el-col>
+           <el-col :span="3"><h4>conf data</h4></el-col>
+           <el-col :span="8"><h4>reference</h4></el-col>
          </el-row>
          <el-row>
            <el-col :span="2"><h5>Developing <i>drosophila</i> embryos and larvae atlas</h5></el-col>
            <el-col :span="2"><h5>Stereo-seq</h5></el-col>
-           <el-col :span="10">
+           <el-col :span="12">
              <el-row>
                <el-col :span="12">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/E14-16h_a_count_normal_stereoseq.h5ad" target="_blank">E14-16h_a_count_normal_stereoseq.h5ad</a></h5>
                </el-col>
-               <el-col :span="12">
+               <el-col :span="6">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/E14-16h_a.tar.gz" target="_blank">E14-16h_a.tar.gz</a></h5>
+               </el-col>
+               <el-col :span="6">
+                  <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/E14-16h.atlas.json" target="_blank">E14-16h.atlas.json</a></h5>
+                  <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/fixed.json" target="_blank">fixed.json</a></h5>
                </el-col>
              </el-row>
              <el-row>
                <el-col :span="12">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/E16-18h_a_count_normal_stereoseq.h5ad" target="_blank">E16-18h_a_count_normal_stereoseq.h5ad</a></h5>
                </el-col>
-               <el-col :span="12">
+               <el-col :span="6">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/E16-18h_a.tar.gz" target="_blank">E16-18h_a.tar.gz</a></h5>
+               </el-col>
+               <el-col :span="6">
+                  <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/E16-18h.atlas.json" target="_blank">E16-18h.atlas.json</a></h5>
                </el-col>
              </el-row>
              <el-row>
                <el-col :span="12">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/L1_a_count_normal_stereoseq.h5ad" target="_blank">L1_a_count_normal_stereoseq.h5ad</a></h5>
                </el-col>
-               <el-col :span="12">
+               <el-col :span="6">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/L1_a.tar.gz" target="_blank">L1_a.tar.gz</a></h5>
+               </el-col>
+               <el-col :span="6">
+                  <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/L1_a.atlas.json" target="_blank">L1_a.atlas.json</a></h5>
                </el-col>
              </el-row>
              <el-row>
                <el-col :span="12">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/L2_a_count_normal_stereoseq.h5ad" target="_blank">L2_a_count_normal_stereoseq.h5ad</a></h5>
                </el-col>
-               <el-col :span="12">
+               <el-col :span="6">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/L2_a.tar.gz" target="_blank">L2_a.tar.gz</a></h5>
+               </el-col>
+               <el-col :span="6">
+                  <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/L2_a.atlas.json" target="_blank">L2_a.atlas.json</a></h5>
                </el-col>
              </el-row>
              <el-row>
                <el-col :span="12">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/L3_b_count_normal_stereoseq.h5ad" target="_blank">L3_b_count_normal_stereoseq.h5ad</a></h5>
                </el-col>
-               <el-col :span="12">
+               <el-col :span="6">
                  <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/L3_b.tar.gz" target="_blank">L3_b.tar.gz</a></h5>
+               </el-col>
+               <el-col :span="6">
+                  <h5><a href="http://www.bgiocean.com/vt3d_example/download/flysta3d/L3_b.atlas.json" target="_blank">L3_b.atlas.json</a></h5>
                </el-col>
              </el-row>
            </el-col>
-           <el-col :span="10">
-             <h5><a href="https://doi.org/10.1016/j.devcel.2022.04.006" target="_blank">MingyueWang, etc., Developmental Cell. High-resolution 3D spatiotemporal transcriptomic maps of developing Drosophila embryos and larvae </a></h5>
+           <el-col :span="8">
+             <h5><a href="https://doi.org/10.1016/j.devcel.2022.04.006" target="_blank">Wang et al., Developmental Cell, 2022 High-resolution 3D spatiotemporal transcriptomic maps of developing Drosophila embryos and larvae </a></h5>
            </el-col>
          </el-row>
 
          <el-row>
            <el-col :span="2"><h5>Developing human heart atlas</h5></el-col>
            <el-col :span="2"><h5>ST</h5></el-col>
-           <el-col :span="5">
+           <el-col :span="6">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/heart/heart3d.h5ad" target="_blank">heart3d.h5ad</a></h5>
            </el-col>
-           <el-col :span="5">
+           <el-col :span="3">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/heart/mesh.tar.gz" target="_blank">mesh.tar.gz</a></h5>
            </el-col>
-           <el-col :span="10">
-             <h5><a href="https://doi.org/10.1016/j.cell.2019.11.025" target="_blank">Michaela Asp, etc., Cell. A Spatiotemporal Organ-Wide Gene Expression and Cell Atlas of the Developing Human Heart</a></h5>
+           <el-col :span="3">
+                  <h5><a href="http://www.bgiocean.com/vt3d_example/download/heart/heart.atlas.json" target="_blank">heart.atlas.json</a></h5>
+                  <h5><a href="http://www.bgiocean.com/vt3d_example/download/heart/fixed.json" target="_blank">fixed.json</a></h5>
+           </el-col>
+           <el-col :span="8">
+             <h5><a href="https://doi.org/10.1016/j.cell.2019.11.025" target="_blank">Asp, et al., Cell, 2019 A Spatiotemporal Organ-Wide Gene Expression and Cell Atlas of the Developing Human Heart</a></h5>
            </el-col>
          </el-row>
          <el-row>
            <el-col :span="2"><h5>Preoptic hypothalamic atlas</h5></el-col>
            <el-col :span="2"><h5>MERFISH</h5></el-col>
-           <el-col :span="5">
+           <el-col :span="6">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/merfish/hypo_preoptic.h5ad" target="_blank">hypo_preoptic.h5ad</a></h5>
            </el-col>
-           <el-col :span="5">
+           <el-col :span="3">
                   <h5><a href="http://www.bgiocean.com/vt3d_example/download/merfish/mesh.tar.gz" target="_blank">mesh.tar.gz</a></h5>
            </el-col>
-           <el-col :span="10">
-             <h5><a href="http://dx.doi.org/10.1126/science.aau5324" target="_blank">Jeffrey R. Moffitt, etc., Science. Molecular, spatial, and functional single-cell profiling of the hypothalamic preoptic region </a></h5>
+           <el-col :span="3">
+                  <h5><a href="http://www.bgiocean.com/vt3d_example/download/merfish/hypo_preoptic.atlas.json" target="_blank">hypo_preoptic.atlas.json</a></h5>
+                  <h5><a href="http://www.bgiocean.com/vt3d_example/download/merfish/hypo_preoptic.coord.json" target="_blank">hypo_preoptic.coord.json</a></h5>
+                  <h5><a href="http://www.bgiocean.com/vt3d_example/download/merfish/hypo_preoptic.organ.json" target="_blank">hypo_preoptic.organ.json</a></h5>
+           </el-col>
+           <el-col :span="8">
+             <h5><a href="http://dx.doi.org/10.1126/science.aau5324" target="_blank">Moffitt, et al., Science, 2018 Molecular, spatial, and functional single-cell profiling of the hypothalamic preoptic region </a></h5>
+           </el-col>
+         </el-row>
+         <br>
+         <h3>Source codes&#58;</h3>
+         <el-row>
+           <el-col :span="8">
+             <a href="https://github.com/BGI-Qingdao/VT3D" target="_blank">Source codes of VT3D in GitHub</a><br>
+           </el-col>
+           <el-col :span="8">
+             <a href="https://github.com/BGI-Qingdao/VT3D_Browser" target="_blank">Source codes of vt3d_browser in GitHub</a><br>
+           </el-col>
+           <el-col :span="8">
+             <a href="https://github.com/cchd0001/VT3D_Index" target="_blank">Source codes of this homepage in GitHub</a>
            </el-col>
          </el-row>
       </el-tab-pane>
     </el-tabs>
     <hr>
-    <el-footer>
-        <div class='footer'>
-          <el-row>
-            <el-col :span="3">
-                <img src="./assets/bgi.png" class="logo" >
-            </el-col>
-            <el-col :span="3">
-                <img src="./assets/bgi_research.png" class="logo" >
-            </el-col>
-            <el-col :span="3">
-                <img src="./assets/cngb.png" class="logo" >
-            </el-col>
-            <el-col :span="8">
-              <p>For any inquiries about the website, please contact: guolidong@genomics.cn; liyao1@genomics.cn</p>
-              <p>Powered by Element.js framework and Echarts.js library</p>
-            </el-col>
-            <el-col :span="7">
-                <a href="https://github.com/BGI-Qingdao/VT3D" target="_blank">Source codes of VT3D in GitHub</a><br>
-                <a href="https://github.com/BGI-Qingdao/VT3D_Browser" target="_blank">Source codes of vt3d_browser in GitHub</a><br>
-                <a href="https://github.com/cchd0001/VT3D_Index" target="_blank">Source codes of this homepage in GitHub</a>
-            </el-col>
-          </el-row>
-        </div>
-    </el-footer>
+    <div class='footer'>
+      <el-row>
+        <el-col :span="3">
+            <img src="./assets/bgi.png" class="logo" >
+        </el-col>
+        <el-col :span="3">
+            <img src="./assets/bgi_research.png" class="logo" >
+        </el-col>
+        <el-col :span="3">
+            <img src="./assets/stomics.png" class="logo" >
+        </el-col>
+        <el-col :span="3">
+            <img src="./assets/stomicsdb.png" class="logo" >
+        </el-col>
+        <el-col :span="3">
+            <img src="./assets/cngb.png" class="logo" >
+        </el-col>
+        <el-col :span="9">
+          <p>For any inquiries about the website, please contact: guolidong@genomics.cn; liyao1@genomics.cn</p>
+          <p>Powered by Element.js and Echarts.js</p>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <p>Copyright &#169; All Rights Reserved 2022 BGI Research <a href=https://beian.miit.gov.cn/>鲁ICP备2022034375号</a></p>
+        </el-col>
+      </el-row>
+    </div>
 </div>
 </div>
 </template>
@@ -600,21 +645,23 @@ export default {
     font-weight: bold;
     color: white;
   }
-  #header-col2 {
-    background-color: #4381e6;
-    text-align: center;
-    color: white;
-  }
   .logo{
     width: 100%;
     display: block;
+    margin:10px;
+    padding:20px;
+  }
+  .footer{
+    border-color: #eeeeee;
+    border-style: solid;
+    border-width: 1px;
+    color: #8b8c8f;
+    font-size: 14px;
+    padding:10px;
   }
   .image {
     width: 100%;
     display: block;
-  }
-  .footer{
-    background-color: white;
   }
   .card{
      border-width: 2px;
@@ -631,13 +678,8 @@ export default {
      margin:10px;
      padding:10px;
   }
-  .el-footer {
-    background-color: white;
-    color: #8b8c8f;
-    font-size: 14px;
-    text-align: center;
-    margin-top: 20px;
-    width: 100%;
+  .left_border{
+     border-left: 3px dashed gray;
   }
   .center-div
   {
